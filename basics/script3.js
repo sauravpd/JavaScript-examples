355464ae6167ddf03093db073682d56ec385ce33
saurav <sauravpd58@gmail.com>
2018-11-11 19:43:27 +0530

    added script3.js

---
+++ basics/script3.js
@@ -0,0 +1,19 @@
+/*
+ * Basic Operators
+ */
+var year,yearSteve,yearJohn;
+year=2018;
+ageJohn=20;
+ageSteve=26;
+// Math Operations
+yearSteve=year-ageSteve;
+yearJohn=year-ageJohn;
+
+console.log(yearSteve)
+console.log(yearJohn)
+
+// logicalOperator
+var SteveOlder=yearSteve<yearJohn;
+console.log(SteveOlder);
+
+console.log(typeof yearSteve)
