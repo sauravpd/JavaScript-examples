db97b4f0fa6c2ba9b8eb4772e62d8fe09361bf09
saurav <sauravpd58@gmail.com>
2018-11-11 16:54:24 +0530

    commit changes in script2

--- basics/script2.js
+++ basics/script2.js
@@ -1,6 +1,13 @@
-var firstName="Steve";
-var lastName="Smith";
-var age=25;
+/*
+ * ********************************
+ * Javascipt variable and datatypes
+ */
+
+// javascript variable
+var firstName = "Steve";
+var lastName = "Smith";
+var age = 25;
+
 console.log(firstName);
 console.log(lastName);
 console.log(age);
