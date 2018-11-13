b415da5eb5c4cbfc316142d42fdc684d3a06cf75
saurav <sauravpd58@gmail.com>
2018-11-14 00:14:44 +0530

    added script6.jd

---
+++ basics/script6.js
@@ -0,0 +1,11 @@
+var percentage = 60;
+
+if (percentage >= 75) {
+	console.log("Distinction");
+} else if (percentage >= 60 && percentage < 75) {
+	console.log("First class");
+} else if (percentage < 60 && percentage >= 35) {
+	console.log("second class")
+} else {
+	console.log("Fail")
+}
