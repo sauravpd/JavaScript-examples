0926b015edd7ccd510c2f42041b12e2414799803
saurav <sauravpd58@gmail.com>
2018-11-18 14:14:18 +0530

    added arrays/script1.js

---
+++ arrays/script1.js
@@ -0,0 +1,11 @@
+var city = [ 'Mumbai', 'Pune', 'Delhi', 'Banglore' ];
+
+var years = new Array(1995, 1996, 19997, 19998, 1999, 2000);
+
+
+console.log(city);
+console.log(years);
+console.log(city.length);
+console.log(years.length);
+
+console.log(city[1]);
