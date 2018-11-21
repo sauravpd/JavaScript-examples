526bd40efc5d1bb2c63f3509b4bfcdc00af081a6
saurav <sauravpd58@gmail.com>
2018-11-22 00:08:25 +0530

    added basics/script8.js

---
+++ basics/script8.js
@@ -0,0 +1,30 @@
+var day = 5;
+
+switch (day) {
+case 1:
+	dayOftheWeek = "Monday";
+	break;
+case 2:
+	dayOftheWeek = "Tuesday";
+	break;
+case 3:
+	dayOftheWeek = "Wednesday";
+	break;
+case 4:
+	dayOftheWeek = "Thursday";
+	break;
+case 5:
+	dayOftheWeek = "Friday";
+	break;
+case 6:
+	dayOftheWeek = "Saturday";
+	break;
+case 7:
+	dayOftheWeek = "Sunday";
+	break;
+default:
+	dayOftheWeek = "Invalid day";
+	break;
+}
+
+console.log(dayOftheWeek);
