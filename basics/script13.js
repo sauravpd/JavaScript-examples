d648b95ac80c71560483885d40613709ae507149
saurav <sauravpd58@gmail.com>
2018-11-30 22:08:05 +0530

    added basic/script13.js

---
+++ basics/script13.js
@@ -0,0 +1,12 @@
+var data = [ 'steve', 'john', 50, true, 25 ]
+
+for (var i = 0; i < data.length; i++) {
+	console.log(data[i])
+}
+
+console.log("**********************************")
+var i = 0;
+while (i < data.length) {
+	console.log(data[i]);
+	i++;
+}
