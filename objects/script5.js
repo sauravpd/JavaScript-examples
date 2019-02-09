a97eddafbc0a4034dd28f3e9f3974b5fc9b8f79e
saurav <sauravpd58@gmail.com>
2019-02-10 00:29:52 +0530

    commit changes

--- objects/script5.js
+++ objects/script5.js
@@ -9,12 +9,26 @@ class User{
 	{
 		return this.firstName;
 	}
+	
+	getLastName()
+	{
+		return this.lastName
+	}
+	
+	getAge()
+	{
+		return this.age;
+	}
 }
 
 const steve=new User('Steve','Anderson',25);
 console.log(steve)
 
+console.log('****************************')
 console.log(steve.getFirstName());
+console.log(steve.getLastName());
+console.log(steve.getAge())
+console.log('****************************')
 // const Mike=new User();
 // console.log(Mike);//undefined
 
