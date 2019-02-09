403500cf97aac782e409c1f1756b7cb6eec073b0
saurav <sauravpd58@gmail.com>
2019-02-10 00:26:53 +0530

    added getFirstName method

--- objects/script5.js
+++ objects/script5.js
@@ -1,15 +1,20 @@
-class User
-{
-	constructor(firstName,lastName,age)
-	{
+class User{
+	constructor(firstName,lastName,age){
 	this.firstName=firstName;
 	this.lastName=lastName;
 	this.age=age;
+	}
+	
+	getFirstName()
+	{
+		return this.firstName;
 	}
 }
 
 const steve=new User('Steve','Anderson',25);
 console.log(steve)
 
-const Mike=new User();
-console.log(Mike);//undefined
+console.log(steve.getFirstName());
+// const Mike=new User();
+// console.log(Mike);//undefined
+
