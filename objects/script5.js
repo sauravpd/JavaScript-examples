88f7130db4cf413db932c8f7e0c46d64a2e23052
saurav <sauravpd58@gmail.com>
2019-02-09 23:52:58 +0530

    added constuctor in script5.js

--- objects/script5.js
+++ objects/script5.js
@@ -1,10 +1,12 @@
 class User
 {
-	constructor()
+	constructor(firstName,lastName,age)
 	{
-	
+	this.firstName=firstName;
+	this.lastName=lastName;
+	this.age=age;
 	}
 }
 
-const steve=new User();
+const steve=new User('Steve','Anderson',25);
 
