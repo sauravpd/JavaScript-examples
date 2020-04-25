94e3854d6c0cab056759029651a1fbe6f0b54f64
saurav <sauravpd58@gmail.com>
2020-04-25 13:15:46 +0530

    added objects and methods example

---
+++ objects/script4.js
@@ -0,0 +1,12 @@
+/*
+*Object and methods 
+*/
+var studentObj={
+    firstName: 'John',
+    lastName: 'Smith',
+    show:function(){
+        console.log("Firstname "+this.firstName)
+    }
+}
+
+studentObj.show();
