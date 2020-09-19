9102e458775a9a7fdf08374fb82fc6acda7c6d90
saurav <sauravpd58@gmail.com>
2020-09-19 18:44:29 +0530

    commit changes"

---
+++ objects/script9.js
@@ -0,0 +1,13 @@
+var employee={
+    name: "Steve",
+    age:25
+}
+
+console.log(employee.age)
+console.log(employee.name)
+
+employee.name="John"
+employee.age=20
+console.log(employee)
+
+console.log(employee['name'])
