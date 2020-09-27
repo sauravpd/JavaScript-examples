c580c6c1e62b1dd63394507c2aeb07914ba06185
saurav <sauravpd58@gmail.com>
2020-09-27 23:52:05 +0530

    added js object example

---
+++ objects/script10.js
@@ -0,0 +1,12 @@
+class Employee{
+
+    constructor(name,age)
+    {
+        this.name=name;
+        this.age=age;
+    }
+
+}
+const empObj=new Employee("Steve",25);
+console.log(empObj.name)
+console.log(empObj.age)
