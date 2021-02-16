b775a9d5ade71ec0b18eefd4f98650ad04d52746
saurav <sauravpd58@gmail.com>
2021-02-17 02:19:50 +0530

    commit changes

---
+++ basics/script15.js
@@ -0,0 +1,27 @@
+/**
+ * hoisting
+ */
+
+ //fuction declaration
+calAge(1995);
+
+ function calAge(year) {
+     console.log(2020-year);
+ }
+
+ 
+//ret(1990);
+//  function expression
+ var ret=function calRet(year) {
+    console.log(65-(2020-year));
+}
+
+
+// variable
+console.log(a)
+var a=20;
+
+function foo()
+{
+    
+}
