8d4b73966974c39eca66b0a32ead6e90c30ca252
saurav <sauravpd58@gmail.com>
2021-02-20 04:05:37 +0530

    commit changes in script10.js

--- objects/script10.js
+++ objects/script10.js
@@ -6,7 +6,18 @@ class Employee{
         this.age=age;
     }
 
+    getName() {
+        return this.name;
+    }
+
+    getAge() {
+        return this.age;
+    }
+
 }
 const empObj=new Employee("Steve",25);
 console.log(empObj.name)
 console.log(empObj.age)
+
+console.log(empObj.getName)
+console.log(empObj.getAge)
